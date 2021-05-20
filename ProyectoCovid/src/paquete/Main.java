e01ee6e6e8980834d42df8683ec8c6ad513b7220
esther <esther@esther-VirtualBox>
2021-05-20 09:20:25 +0200

    Primera versión del proyecto. Solo contiene un saludo.

---
+++ ProyectoCovid/src/paquete/Main.java
@@ -0,0 +1,16 @@
+package paquete;
+
+/**
+ * 
+ * @author esther
+ * @version 1.0.0
+ * 
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println("PROBANDO...");
+		
+	}
+}
